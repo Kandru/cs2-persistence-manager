bfbe34c2e1a8eac6a08bad7da6686d4a90e6242f
Kalle <info@kalleminkner.de>
2025-03-21 09:01:03 +0100

    fix: remove unnecessary data

--- src/PersistenceManager.cs
+++ src/PersistenceManager.cs
@@ -1,12 +1,8 @@
-using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
-using CounterStrikeSharp.API.Modules.Utils;
-using System.Globalization;
-using System.Text.Json.Serialization;
 
 namespace PersistenceManager
 {
-    public partial class PersistenceManager : BasePlugin, IPluginConfig<PluginConfig>
+    public partial class PersistenceManager : BasePlugin
     {
         public override string ModuleName => "CS2 PersistenceManager";
         public override string ModuleAuthor => "Originally by Franc1sco Franug / rewritten by Jon-Mailes Graeffe <mail@jonni.it> and Kalle <kalle@kandru.de>";
